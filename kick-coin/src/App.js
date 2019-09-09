8a72d48ea6101422a4951774ab70493ede14355c
keenanhammonds <keenan.hammonds@gmail.com>
2019-09-09 12:14:40 -0400

    restarted front-end with react material ui

---
+++ kick-coin/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar color="primary" position="static">
+        <h1>Kick-Coin</h1>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
