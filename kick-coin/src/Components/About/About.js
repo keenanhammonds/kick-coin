7420050a74393aab02c499223cc7f423b42e798c
keenanhammonds <keenan.hammonds@gmail.com>
2019-09-10 10:55:34 -0400

    css changes as well as form changes for add

--- kick-coin/src/Components/About/About.js
+++ kick-coin/src/Components/About/About.js
@@ -1,17 +1,31 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
+import photoOne from "./photo-one.png"
+import photoTwo from "./photo-two.jpg"
+import photoThree from "./photo-three.png"
 
-class About extends Component{
-    render(){
-        return(
-            <div className="about-container">
-                <p>Kick Coin is for the hustlers.</p>
-                <p className="text-item">Kick Coin connects and encourages streetwear enthusiasts, fashionistas, trend setters & artists <br/>
-                with cryptocurrency in a simple way. How much is your gear worth in cryptocurrency? <br />
-                Resellers and buyers have always known sneakers can be an investment, Kick-Coin furthers that philosophy.
-                 </p>
-            </div>
-        )
-    }
+class About extends Component {
+  render() {
+    return (
+      <div className="main-about-container">
+        <div className="about-container">
+          <h3 className="hustlers">Kick Coin is for the hustlers.</h3>
+          <p className="text-item">
+            Kick Coin connects and encourages streetwear enthusiasts,
+            fashionistas, trend setters & artists <br />
+            with cryptocurrency in a simple way. How much is your gear worth in
+            cryptocurrency? <br />
+            Resellers and buyers have always known sneakers can be an
+            investment, Kick-Coin furthers that philosophy.
+          </p>
+        </div>
+        <div>
+            <img id="photo-one" src={photoOne} />
+            <img id="photo-two" src={photoTwo} />
+            <img id="photo-three" src={photoThree}/>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
--- kick-coin/src/Components/Add/Add.js
+++ kick-coin/src/Components/Add/Add.js
@@ -1,28 +1,50 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
-class Add extends Component{
-    constructor(){
-        super();
-        this.state = {
-            shoeName: ''
-        }
-    }
-  
-    shoeChangeHandler = event => {
-        this.setState({
-          shoeName: event.target.value
-        });
-        console.log(this.state)
-      }
+class Add extends Component {
+  constructor() {
+    super();
+    this.state = {
+      shoeName: ""
+    };
+  }
 
-    render(){
-        return(
-            <div className="form-container">
-                <div className="form-item"><input className="form-input" onChange={this.shoeChangeHandler} type="text" placeholder="Shoe Name"></input></div>
-               <div className="form-item"><input className="form-input" onChange={this.urlChangeHandler} type="text" placeholder="where's the picture?" ></input></div>
-            </div>
-        )
-    }
+  shoeChangeHandler = event => {
+    this.setState({
+      shoeName: event.target.value
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="form-container">
+        <div className="form-item">
+          <input
+            className="form-input"
+            onChange={this.shoeChangeHandler}
+            type="text"
+            placeholder="Shoe Name"
+          ></input>
+        </div>
+        <div className="form-item">
+          <input
+            className="form-input"
+            onChange={this.urlChangeHandler}
+            type="text"
+            placeholder="where's the picture?"
+          ></input>
+        </div>
+        <div className="form-item">
+          <input
+            className="form-input"
+            onChange={this.urlChangeHandler}
+            type="text"
+            placeholder="where's the picture?"
+          ></input>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Add
+export default Add;
