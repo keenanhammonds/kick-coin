9961f4727b23b0c866a9881f7ccfab990c8e8bae
keenanhammonds <keenan.hammonds@gmail.com>
2019-09-09 22:33:19 -0400

    updated forms in add

--- kick-coin/src/Components/Add/Add.js
+++ kick-coin/src/Components/Add/Add.js
@@ -1,10 +1,25 @@
 import React, {Component} from "react"
 
 class Add extends Component{
+    constructor(){
+        super();
+        this.state = {
+            shoeName: ''
+        }
+    }
+  
+    shoeChangeHandler = event => {
+        this.setState({
+          shoeName: event.target.value
+        });
+        console.log(this.state)
+      }
+
     render(){
         return(
-            <div>
-                <h1>Add A Shoe!</h1>
+            <div className="form-container">
+                <div className="form-item"><input className="form-input" onChange={this.shoeChangeHandler} type="text" placeholder="Shoe Name"></input></div>
+               <div className="form-item"><input className="form-input" onChange={this.urlChangeHandler} type="text" placeholder="where's the picture?" ></input></div>
             </div>
         )
     }
